dca8df78e964370c549ff754b6782ff10e0404b3
Mark <mutofifin@mail.ru>
2022-08-26 20:45:37 +0300

    13 задание

---
+++ 13.py/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число, а я покажу его третью цифру или скажу, что ее нет!");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+    Console.WriteLine("Третьей цифры нет :(");
+else
+    Console.WriteLine(Convert.ToString(n)[2]);
