a7ca931afca9d196a8a223f8c47bfc9c817d8441
Mark <mutofifin@mail.ru>
2022-08-26 20:43:40 +0300

    10 задание

---
+++ exs10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 999)
+{
+    Console.WriteLine("Я сказал ТРЕХзначное, попробуй еще раз");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine((n / 10) % 10);
+
