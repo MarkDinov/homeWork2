0b1d65240f19cf2130df82435e89843a38fab04e
Mark <mutofifin@mail.ru>
2022-08-26 20:44:42 +0300

    15 задание

---
+++ 15.py/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.WriteLine("Это не является днем недели");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 6 || n == 7)
+    Console.WriteLine("Отдыхай!");
+else
+    Console.WriteLine("Работай!");
